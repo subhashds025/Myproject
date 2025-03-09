a1d1a8670fe22f8cf48eb6afe6ff60e713a893ef
subhash <subhashds025@gmail.com>
2025-03-10 01:26:07 +0530

    this is my testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Lazy loading the UserModule
+const routes: Routes = [
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
